4b8e3bd3a138eff67acc8c707cc1f30f6c016bad
Bogdan Pilch <bogdan.pilch@whiteops.com>
2022-08-25 11:09:57 +0200

    sample main func

--- cmd/message_bird/main.go
+++ cmd/message_bird/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"github.com/bagadatp/message_bird/pkg/sample"
+)
 func main() {
 
+	input := 10
+	result := sample.IntToString(input)
+	fmt.Printf("Converted input int %v to string '%v'\n", input, result)
 }
